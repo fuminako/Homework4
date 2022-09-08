a659915555da5c1d6d7f04ebd2a0cad4a3816022
Анастасия <fumi_nako@mail.ru>
2022-09-08 04:38:41 +0300

    ДЗ Задание 7, 8, 9.

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
         }
 
         // Задание 2
-        int yearsOld = 7;
+        int yearsOld = 24;
         if ( yearsOld >= 7 && yearsOld < 18) {
             System.out.println("Ребёнок ходит в школу");
         } else if (yearsOld >= 18 && yearsOld < 24) {
             System.out.println("Человек закончил школу и и может отправляться в университет");
-        } else {
+        } else if (yearsOld >= 24){
             System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
 
@@ -26,6 +26,42 @@ public class Main {
             System.out.println("Остались только стоячие места");
         } else {
             System.out.println("Мест в вагоне не осталось");
+        }
+
+        // Задание 4
+
+        int humanAge = 1;
+        if (humanAge >=2 && humanAge < 6 ) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в детский сад");
+        } else if (humanAge >= 7 && humanAge <= 18) {
+            System.out.println("Eсли возраст человека равен " + humanAge + ", то ему нужно ходить в школу");
+        } else if (humanAge > 18 && humanAge <= 24 ) {
+            System.out.println("Eсли возраст человека равен " + humanAge + ", то ему нужно ходить в университет");
+        } else if (humanAge > 24){
+            System.out.println("Eсли возраст человека равен " + humanAge + ", то ему нужно ходить на работу");
+        }
+
+        // Задание 5
+
+        int theAgeOfTheChild = 3;
+        if ( theAgeOfTheChild < 5 ) {
+            System.out.println("Ребёнок не может кататься на аттракционе");
+        } else if ( theAgeOfTheChild >= 5 && theAgeOfTheChild < 14 ) {
+            System.out.println("Ребёнок может кататься на аттракционе только в сопровождении взрослого");
+        } else {
+            System.out.println("Ребёнок может кататься на аттракционе без сопровождения взрослого");
+        }
+
+        // Задание 6
+        int one = 7;
+        int two = 1;
+        int free = 3;
+        if ( one > two && one > free) {
+            System.out.println(one);
+        } else if ( two > free) {
+            System.out.println(two);
+        } else {
+            System.out.println(free);
         }
     }
 }
